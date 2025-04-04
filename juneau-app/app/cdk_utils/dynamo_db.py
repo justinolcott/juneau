6e5ef55ab342661229bb4967bf136f92c8841dc9
DrewGalbraith <95897287+DrewGalbraith@users.noreply.github.com>
2025-04-03 22:43:47 -0600

    Alter billing procedure

--- juneau-app/app/cdk_utils/dynamo_db.py
+++ juneau-app/app/cdk_utils/dynamo_db.py
@@ -5,7 +5,7 @@ class DynamoDBTable(Construct):
     def __init__(self, scope: Construct, id: str, table_name: str, **kwargs) -> None:
         super().__init__(scope, id)
 
-        self.table = dynamodb.Table(
+        self.table = dynamodb.TableV2(
             self,
             f"{id}_Table",
             table_name=table_name,
@@ -13,5 +13,9 @@ class DynamoDBTable(Construct):
                 name="id",
                 type=dynamodb.AttributeType.STRING
             ),
-            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+            # billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST# might want this later
+            billing_mode=dynamodb.Billing.on_demand(
+                max_read_request_units=25,  # 25/sec is the free tier limit
+                max_write_request_units=25  # 25/sec is the free tier limit
+            ),
         )
