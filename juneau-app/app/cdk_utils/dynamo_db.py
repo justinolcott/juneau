b4a9e903867673757cc2329174534ade250ba6bc
DrewGalbraith <95897287+DrewGalbraith@users.noreply.github.com>
2025-04-08 13:03:46 -0600

    Adjust billing to accept 'billing' kwarg

--- juneau-app/app/cdk_utils/dynamo_db.py
+++ juneau-app/app/cdk_utils/dynamo_db.py
@@ -9,17 +9,26 @@ class DynamoDBTable(Construct):
     def __init__(self, scope: Construct, id: str, table_name: str, **kwargs) -> None:
         super().__init__(scope, id)
 
+
         self.table = dynamodb.TableV2(
-            self,
+            scope=self,  # set table's scope to `DynamoDBTable` construct
             f"{id}_Table",
             table_name=table_name,
             partition_key=dynamodb.Attribute(
                 name="id",
                 type=dynamodb.AttributeType.STRING
             ),
+            sort_key=dynamodb.Attribute(
+                name='',
+                tpye=dynamodb.AttrinuteType.NUMBER
+            )
             # billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST# might want this later
-            billing_mode=dynamodb.Billing.on_demand(
-                max_read_request_units=25,  # 25/sec is the free tier limit
-                max_write_request_units=25  # 25/sec is the free tier limit
-            ),
+
+            # Set billing standards
+            billing_mode=kwargs.get("billing", 
+                dynamodb.Billing.on_demand(
+                            max_read_request_units=25,  # 25/sec is the free tier limit
+                            max_write_request_units=25  # 25/sec is the free tier limit
+                ),
+            )
         )
